4ed30d4a54ba417a2c6d4e5abfb0efc007e1b4ee
ShenSouza <ds052798ts@gmail.com>
2021-02-21 14:36:56 -0300

    Commit inicial

---
+++ src/test/java/com/denilson/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denilson.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
