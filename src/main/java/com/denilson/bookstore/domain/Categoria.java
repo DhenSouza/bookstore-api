a8ee09abd075906cb3b8da3769dd9fadb9fbdd3a
ShenSouza <ds052798ts@gmail.com>
2021-02-22 14:28:01 -0300

    Inserindo categoria e livros no BD

--- src/main/java/com/denilson/bookstore/domain/Categoria.java
+++ src/main/java/com/denilson/bookstore/domain/Categoria.java
@@ -10,7 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-@Entity
+@Entity(name = "Categoria")
 public class Categoria implements Serializable{
 
 	private static final long serialVersionUID = 1L;
--- src/main/java/com/denilson/bookstore/repositories/LivroRepository.java
+++ src/main/java/com/denilson/bookstore/repositories/LivroRepository.java
@@ -1,7 +1,7 @@
 package com.denilson.bookstore.repositories;
 
-import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.denilson.bookstore.domain.Livro;
 
