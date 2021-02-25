e665fe263daf8655de472e7a1c722d31858a60c3
ShenSouza <ds052798ts@gmail.com>
2021-02-25 18:36:01 -0300

    Tratado erro Notexception com menssagem

--- src/main/java/com/denilson/bookstore/service/CategoriaService.java
+++ src/main/java/com/denilson/bookstore/service/CategoriaService.java
@@ -7,17 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.denilson.bookstore.domain.Categoria;
 import com.denilson.bookstore.repositories.CategoriaRepository;
+import com.denilson.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repository;
-	
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		
-		return obj.orElse(null);
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado: " + id + " Tipo: " + Categoria.class.getName()));
 	}
 }
---
+++ src/main/java/com/denilson/bookstore/service/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,16 @@
+package com.denilson.bookstore.service.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+	public ObjectNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ObjectNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
