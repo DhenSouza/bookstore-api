e46a118fcf3b11c27cf14d80c0041fa209c1b6e4
ShenSouza <ds052798ts@gmail.com>
2021-03-04 20:15:02 -0300

    feat(Service,Resource): Criado método UPDATE #2

--- src/main/java/com/denilson/bookstore/service/CategoriaService.java
+++ src/main/java/com/denilson/bookstore/service/CategoriaService.java
@@ -3,10 +3,13 @@ package com.denilson.bookstore.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.denilson.bookstore.domain.Categoria;
+import com.denilson.bookstore.dtos.CategoriaDTO;
 import com.denilson.bookstore.repositories.CategoriaRepository;
 import com.denilson.bookstore.service.exceptions.ObjectNotFoundException;
 
@@ -22,13 +25,20 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado: " + id + " Tipo: " + Categoria.class.getName()));
 	}
-	
-	public List<Categoria> findAll(){
+
+	public List<Categoria> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDto) {
+		Categoria obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setDescricao(objDto.getDescricao());
 		return repository.save(obj);
 	}
 }
