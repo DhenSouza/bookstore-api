08788baf5aa26e4cb178eb06602396b598e93b79
DhenSouza <ds052798ts@gmail.com>
2021-05-31 17:41:35 -0300

    feat(Repository): Implementado novo metodo em Repositori #9

--- src/main/java/com/denilson/bookstore/resources/LivroResource.java
+++ src/main/java/com/denilson/bookstore/resources/LivroResource.java
@@ -1,13 +1,13 @@
 package com.denilson.bookstore.resources;
 
 import com.denilson.bookstore.domain.Livro;
+import com.denilson.bookstore.dtos.LivroDTO;
 import com.denilson.bookstore.service.LivroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/livros")
@@ -22,5 +22,11 @@ public class LivroResource {
     Livro obj = service.findById(id);
 
     return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LivroDTO>> findAll(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat) {
+        // localhost:8080/livros?categoria=1
+        List<Livro> list = service.findAll(id_cat);
     }
 }
