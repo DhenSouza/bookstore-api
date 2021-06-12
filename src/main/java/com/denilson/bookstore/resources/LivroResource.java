6321741cd3b1d6a5d459ae3cf651589ee3be359e
Denilson Souza <denilson_contato@outlook.com>
2021-06-12 13:43:15 -0300

    feat(resources, service): Implementado merodo delete para livros #12

--- src/main/java/com/denilson/bookstore/resources/LivroResource.java
+++ src/main/java/com/denilson/bookstore/resources/LivroResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,42 +26,53 @@ import com.denilson.bookstore.service.LivroService;
 @RequestMapping("/livros")
 public class LivroResource {
 
-    @Autowired
-    private LivroService service;
+	@Autowired
+	private LivroService service;
 
-    // Método para a Procura pelo Id de livro.
-    @GetMapping("/{id}")
-    public ResponseEntity<Livro> findById(@PathVariable Integer id) {
-    Livro obj = service.findById(id);
+	// Método para a Procura pelo Id de livro.
+	@GetMapping("/{id}")
+	public ResponseEntity<Livro> findById(@PathVariable Integer id) {
+		Livro obj = service.findById(id);
 
-    return ResponseEntity.ok().body(obj);
-    }
+		return ResponseEntity.ok().body(obj);
+	}
 
-    @GetMapping
-    public ResponseEntity<List<LivroDTO>> findAll(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat) {
-        // localhost:8080/livros?categoria=1
-        List<Livro> list = service.findAll(id_cat);
-        List<LivroDTO> listDTO = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
-        return ResponseEntity.ok().body(listDTO);
-    }
-    
-    // Atualiza toda a informação da entidade
-    @PutMapping(value="/{id}")
-    public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro obj) {
-    	Livro newObj = service.update(id, obj);
-    	return ResponseEntity.ok().body(newObj);
-    }
-    // Atualiza somente uma informação da entidade
-    @PatchMapping(value="/{id}")
-    public ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @RequestBody Livro obj) {
-    	Livro newObj = service.update(id, obj);
-    	return ResponseEntity.ok().body(newObj);
-    }
-    
-    @PostMapping
-    public ResponseEntity<Livro> create(@RequestParam(value="categoria", defaultValue = "0") Integer id_cat, @RequestBody Livro obj) {
-    	Livro newObj = service.create(id_cat, obj);
-    	URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/livros/{id}").buildAndExpand(newObj.getId()).toUri();
-    	return ResponseEntity.created(uri).build();
-    }
+	@GetMapping
+	public ResponseEntity<List<LivroDTO>> findAll(
+			@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat) {
+		// localhost:8080/livros?categoria=1
+		List<Livro> list = service.findAll(id_cat);
+		List<LivroDTO> listDTO = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+
+	// Atualiza toda a informação da entidade
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro obj) {
+		Livro newObj = service.update(id, obj);
+		return ResponseEntity.ok().body(newObj);
+	}
+
+	// Atualiza somente uma informação da entidade
+	@PatchMapping(value = "/{id}")
+	public ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @RequestBody Livro obj) {
+		Livro newObj = service.update(id, obj);
+		return ResponseEntity.ok().body(newObj);
+	}
+
+	@PostMapping
+	public ResponseEntity<Livro> create(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat,
+			@RequestBody Livro obj) {
+		Livro newObj = service.create(id_cat, obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/livros/{id}")
+				.buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Livro> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
 }
